c3de5f701e623ccbbe717187d70d0c6aa818bb4d
showk.mahmoud <showk.mahmoud@3ddx.com>
2024-01-09 11:23:19 +0400

    CR: adding more data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home/home.component';
 import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
--- src/app/shared/Static Data/data.ts
+++ src/app/shared/Static Data/data.ts
@@ -15,4 +15,14 @@ export const data: ITeam[] = [
       { name: 'Player 2', checked: true },
     ],
   },
+  {
+    name: 'Team 3',
+    players: [
+      { name: 'Player 1', checked: true },
+      { name: 'Player 2', checked: false },
+      { name: 'Player 3', checked: false },
+      { name: 'Player 4', checked: true },
+
+    ],
+  },
 ];
